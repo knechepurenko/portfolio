5c2bb95c586b8402052215a9fb2a8a96767cff89
knechepurenko <retard15810@gmail.com>
2021-07-26 04:05:57 +0300

    continue js

--- build/app.js
+++ build/app.js
@@ -1,1 +1,13 @@
-console.log(321)
+const form = document.querySelector('#my-form');
+form.addEventListener('submit', function (e){
+    e.preventDefault()
+    console.log(e)
+    const formData = new FormData(e.target)
+    fetch('https://jsonplaceholder.typicode.com/comments?postID=1')
+        .then(function (response ){
+            return response.json();
+        })
+        .then(function(json){
+            console.log(json)
+        })
+});
